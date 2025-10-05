aaa1220056a2ad9a951795b4ed4cc744de68d83e
Andy Torres <andy18wrld@gmail.com>
2025-10-05 14:45:20 -0400

    fix:Modificado el home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { Experience } from '@/components/experience/Experience';
+import { GetInTouch } from '@/components/getInTouch/GetInTouch';
 import { HeaderComponent } from '@/components/header/Header';
 import { Hero } from '@/components/Hero/Hero';
+import { Projects } from '@/components/proje/Experience-Project';
+
+import { TechStack } from '@/components/Tech-Stack/Tech-Stack';
 
 export const Home = () => {
   return (
@@ -11,6 +16,37 @@ export const Home = () => {
       <main className='flex-1 mt-20'>
         <section className=' flex items-center justify-center relative overflow-hidden px-4'>
           <Hero />
+        </section>
+        {/* <section className='py-24 sm:py-32 relative overflow-hidden  '>
+          <About />
+        </section>*/}
+
+        <section
+          id='Experiencia'
+          className=' py-24 sm:py-32 relative overflow-hidden'
+        >
+          <Experience />
+        </section>
+
+        <section
+          id='Tecnologías'
+          className='py-24 sm:py-32 relative overflow-hidden'
+        >
+          <TechStack />
+        </section>
+
+        <section
+          id='Proyecto'
+          className='py-24 sm:py-32 relative overflow-hidden'
+        >
+          <Projects />
+        </section>
+
+        <section
+          id='Contáctame'
+          className='py-24 sm:py-32 relative overflow-hidden'
+        >
+          <GetInTouch />
         </section>
       </main>
 
