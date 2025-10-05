6a3550f38e1dab447e6f542610371405946788b5
Andy Torres <andy18wrld@gmail.com>
2025-10-05 02:39:32 -0400

    Agregada imagenes y iconos

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,28 @@
+import ReactIcon from './React.svg';
+import AngularIcon from './Angular.svg';
+import DockerIcon from './Docker.svg';
+import GitIcon from './Git.svg';
+import JavaScriptIcon from './JavaScript.svg';
+import TypeScriptIcon from './TypeScript.svg';
+import PostgreSQLIcon from './PostgresSQL.svg';
+import TailwindCSSIcon from './TailwindCSS.svg';
+import HTMLIcon from './HTML5.svg';
+import CSSIcon from './CSS3.svg';
+import PostmanIcon from './Postman.svg';
+import NestIcon from './Nest.js.svg';
+
+
+export {
+  HTMLIcon,
+  CSSIcon,
+  PostmanIcon,
+  ReactIcon,
+  AngularIcon,
+  DockerIcon,
+  GitIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  PostgreSQLIcon,
+  TailwindCSSIcon,
+  NestIcon
+};
